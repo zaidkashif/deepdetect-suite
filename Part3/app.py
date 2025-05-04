fc44539f81de25f04910c73aadc90f029d5ac434
zaidkashif <f223736@cfd.nu.edu.pk>
2025-05-04 22:23:41 +0500

    Saving chnages

--- Part3/app.py
+++ Part3/app.py
@@ -13,8 +13,8 @@ st.set_page_config(page_title="AI Predictive Tools", layout="wide")
 # ===================
 # Define Directories
 # ===================
-PART1_MODEL_DIR = os.path.dirname(__file__)
-PART2_MODEL_DIR = os.path.dirname(__file__)
+PART1_MODEL_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Part1', 'part1_artifacts'))
+PART2_MODEL_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Part2', 'part2_artifacts'))
 
 
 # ===================
